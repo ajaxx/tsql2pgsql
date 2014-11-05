1382c4f04adfcd233e95c0689666d959deb050d2
Aaron Jackson <ajackson@revionics.com>
2014-11-05 15:46:01 -0600

    Adding Pair

---
+++ src/collections/Pair.cs
@@ -0,0 +1,37 @@
+﻿namespace tsql2pgsql.collections
+{
+    /// <summary>
+    /// Construct for pair of objects
+    /// </summary>
+    /// <typeparam name="TA">The type of a.</typeparam>
+    /// <typeparam name="TB">The type of the b.</typeparam>
+    public class Pair<TA,TB>
+    {
+        /// <summary>
+        /// Gets or sets the A value.
+        /// </summary>
+        public TA A { get; set; }
+        /// <summary>
+        /// Gets or sets the B value.
+        /// </summary>
+        public TB B { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Pair{TA, TB}"/> class.
+        /// </summary>
+        public Pair()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Pair{TA, TB}"/> class.
+        /// </summary>
+        /// <param name="a">a.</param>
+        /// <param name="b">The b.</param>
+        public Pair(TA a, TB b)
+        {
+            A = a;
+            B = b;
+        } 
+    }
+}
